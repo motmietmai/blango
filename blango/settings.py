fdaa08a618331d2e699645a615ef728dc050be35
Quang Ngoc <quanganh.pn0910@gmail.com>
2023-11-01 08:53:41 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -46,12 +46,17 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'blango_auth',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
-        "debug_toolbar",
+        'debug_toolbar',
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        'allauth.socialaccount.providers.google'
     ]
 
     MIDDLEWARE = [
@@ -64,6 +69,7 @@ class Dev(Configuration):
         'django.contrib.messages.middleware.MessageMiddleware',
         # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ]
+    SITE_ID = 1
     INTERNAL_IPS = ["192.168.10.93"]
     ROOT_URLCONF = 'blango.urls'
 
@@ -193,7 +199,10 @@ class Dev(Configuration):
     AUTH_USER_MODEL = "blango_auth.User"
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
-
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 class Prod(Dev):
     DEBUG = False
     SECRET_KEY = values.SecretValue()
